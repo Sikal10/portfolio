d26b4150034ce4fd3c3f459bfafdec1d3375d086
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-10-11 14:16:00 +0100

    README updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import ParticlesJs from "../components/ParticlesJs";
+import Navbar from "../components/Navbar";
+import BottomNav from "../components/BottomNav";
+import {useRouter} from "next/router";
+import Title from "../components/Title";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const path = router.pathname;
+
+  return <>
+    <Title title={"Code Sikal | Full-Stack Developer"} />
+    <ParticlesJs />
+    <main className={"dark:bg-black h-screen overflow-hidden z-10"}>
+      <Navbar />
+      <Component {...pageProps} />
+      <BottomNav />
+      {/*{path !== "/works" && <BottomNav />}*/}
+    </main>
+  </>
 }
 
 export default MyApp
