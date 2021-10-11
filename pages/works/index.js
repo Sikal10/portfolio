bc5ef383aa2b9a2ddb69122eab4227af38a4c656
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-10-11 14:09:38 +0100

    My Works

---
+++ pages/works/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Work from "../../components/Work";
+
+const Index = () => {
+    return (
+        <div>
+            <Work />
+        </div>
+    );
+};
+
+export default Index;
